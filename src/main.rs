bd01e899b2b9fd886801b0277455801ca5aeebc2
Beepbop0 <hey.nikhil42@gmail.com>
2021-04-23 15:14:30 -0500

    use dynamic programming function to generate compounds + rustfmt + add tests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use maplit::hashmap;
+use std::collections::HashMap;
 use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use structopt::StructOpt;
-use std::collections::HashMap;
-use maplit::hashmap;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -32,7 +32,6 @@ fn main() {
     } else {
         println!("USAGE: `periodic-words -w <single-word>` or `periodic-words -f <file>`");
     }
-
 }
 
 struct Compound(pub Vec<String>);
@@ -52,166 +51,251 @@ impl fmt::Display for Compound {
 }
 
 struct PeriodicWords {
-    periodic_elements: HashMap<&'static str, &'static str>
+    periodic_elements: HashMap<&'static str, &'static str>,
 }
 
 impl PeriodicWords {
-
     fn new() -> Self {
         Self {
             periodic_elements: hashmap! {
-       "ac" => "Actinium",
-       "ag" => "Silver",
-       "al" => "Aluminium",
-       "am" => "Americium",
-       "ar" => "Argon",
-       "as" => "Arsenic",
-       "at" => "Astatine",
-       "au" => "Gold",
-       "b" => "Boron",
-       "ba" => "Barium",
-       "be" => "Beryllium",
-       "bh" => "Bohrium",
-       "bi" => "Bismuth",
-       "bk" => "Berkelium",
-       "br" => "Bromine",
-       "c" => "Carbon",
-       "ca" => "Calcium",
-       "cd" => "Cadmium",
-       "ce" => "Cerium",
-       "cf" => "Californium",
-       "cl" => "Chlorine",
-       "cm" => "Curium",
-       "cn" => "Copernicium",
-       "co" => "Cobalt",
-       "cr" => "Chromium",
-       "cs" => "Caesium",
-       "cu" => "Copper",
-       "db" => "Dubnium",
-       "ds" => "Darmstadtium",
-       "dy" => "Dysprosium",
-       "er" => "Erbium",
-       "es" => "Einsteinium",
-       "eu" => "Europium",
-       "f" => "Fluorine",
-       "fe" => "Iron",
-       "fl" => "Flerovium",
-       "fm" => "Fermium",
-       "fr" => "Francium",
-       "ga" => "Gallium",
-       "gd" => "Gadolinium",
-       "ge" => "Germanium",
-       "h" => "Hydrogen",
-       "he" => "Helium",
-       "hf" => "Hafnium",
-       "hg" => "Mercury",
-       "ho" => "Holmium",
-       "hs" => "Hassium",
-       "i" => "Iodine",
-       "in" => "Indium",
-       "ir" => "Iridium",
-       "k" => "Potassium",
-       "kr" => "Krypton",
-       "la" => "Lanthanum",
-       "li" => "Lithium",
-       "lr" => "Lawrencium",
-       "lu" => "Lutetium",
-       "lv" => "Livermorium",
-       "mc" => "Moscovium",
-       "md" => "Mendelevium",
-       "mg" => "Magnesium",
-       "mn" => "Manganese",
-       "mo" => "Molybdenum",
-       "mt" => "Meitnerium",
-       "n" => "Nitrogen",
-       "na" => "Sodium",
-       "nb" => "Niobium",
-       "nd" => "Neodymium",
-       "ne" => "Neon",
-       "nh" => "Nihonium",
-       "ni" => "Nickel",
-       "no" => "Nobelium",
-       "np" => "Neptunium",
-       "o" => "Oxygen",
-       "og" => "Oganesson",
-       "os" => "Osmium",
-       "p" => "Phosphorus",
-       "pa" => "Protactinium",
-       "pb" => "Lead",
-       "pd" => "Palladium",
-       "pm" => "Promethium",
-       "po" => "Polonium",
-       "pr" => "Praseodymium",
-       "pt" => "Platinum",
-       "pu" => "Plutonium",
-       "ra" => "Radium",
-       "rb" => "Rubidium",
-       "re" => "Rhenium",
-       "rf" => "Rutherfordium",
-       "rg" => "Roentgenium",
-       "rh" => "Rhodium",
-       "rn" => "Radon",
-       "ru" => "Ruthenium",
-       "s" => "Sulfur",
-       "sb" => "Antimony",
-       "sc" => "Scandium",
-       "se" => "Selenium",
-       "sg" => "Seaborgium",
-       "si" => "Silicon",
-       "sm" => "Samarium",
-       "sn" => "Tin",
-       "sr" => "Strontium",
-       "ta" => "Tantalum",
-       "tb" => "Terbium",
-       "tc" => "Technetium",
-       "te" => "Tellurium",
-       "th" => "Thorium",
-       "ti" => "Titanium",
-       "tl" => "Thallium",
-       "tm" => "Thulium",
-       "ts" => "Tennessine",
-       "u" => "Uranium",
-       "v" => "Vanadium",
-       "w" => "Tungsten",
-       "xe" => "Xenon",
-       "y" => "Yttrium",
-       "yb" => "Ytterbium",
-       "zn" => "Zinc",
-       "zr" => "Zirconium",
-    }
+               "ac" => "Actinium",
+               "ag" => "Silver",
+               "al" => "Aluminium",
+               "am" => "Americium",
+               "ar" => "Argon",
+               "as" => "Arsenic",
+               "at" => "Astatine",
+               "au" => "Gold",
+               "b" => "Boron",
+               "ba" => "Barium",
+               "be" => "Beryllium",
+               "bh" => "Bohrium",
+               "bi" => "Bismuth",
+               "bk" => "Berkelium",
+               "br" => "Bromine",
+               "c" => "Carbon",
+               "ca" => "Calcium",
+               "cd" => "Cadmium",
+               "ce" => "Cerium",
+               "cf" => "Californium",
+               "cl" => "Chlorine",
+               "cm" => "Curium",
+               "cn" => "Copernicium",
+               "co" => "Cobalt",
+               "cr" => "Chromium",
+               "cs" => "Caesium",
+               "cu" => "Copper",
+               "db" => "Dubnium",
+               "ds" => "Darmstadtium",
+               "dy" => "Dysprosium",
+               "er" => "Erbium",
+               "es" => "Einsteinium",
+               "eu" => "Europium",
+               "f" => "Fluorine",
+               "fe" => "Iron",
+               "fl" => "Flerovium",
+               "fm" => "Fermium",
+               "fr" => "Francium",
+               "ga" => "Gallium",
+               "gd" => "Gadolinium",
+               "ge" => "Germanium",
+               "h" => "Hydrogen",
+               "he" => "Helium",
+               "hf" => "Hafnium",
+               "hg" => "Mercury",
+               "ho" => "Holmium",
+               "hs" => "Hassium",
+               "i" => "Iodine",
+               "in" => "Indium",
+               "ir" => "Iridium",
+               "k" => "Potassium",
+               "kr" => "Krypton",
+               "la" => "Lanthanum",
+               "li" => "Lithium",
+               "lr" => "Lawrencium",
+               "lu" => "Lutetium",
+               "lv" => "Livermorium",
+               "mc" => "Moscovium",
+               "md" => "Mendelevium",
+               "mg" => "Magnesium",
+               "mn" => "Manganese",
+               "mo" => "Molybdenum",
+               "mt" => "Meitnerium",
+               "n" => "Nitrogen",
+               "na" => "Sodium",
+               "nb" => "Niobium",
+               "nd" => "Neodymium",
+               "ne" => "Neon",
+               "nh" => "Nihonium",
+               "ni" => "Nickel",
+               "no" => "Nobelium",
+               "np" => "Neptunium",
+               "o" => "Oxygen",
+               "og" => "Oganesson",
+               "os" => "Osmium",
+               "p" => "Phosphorus",
+               "pa" => "Protactinium",
+               "pb" => "Lead",
+               "pd" => "Palladium",
+               "pm" => "Promethium",
+               "po" => "Polonium",
+               "pr" => "Praseodymium",
+               "pt" => "Platinum",
+               "pu" => "Plutonium",
+               "ra" => "Radium",
+               "rb" => "Rubidium",
+               "re" => "Rhenium",
+               "rf" => "Rutherfordium",
+               "rg" => "Roentgenium",
+               "rh" => "Rhodium",
+               "rn" => "Radon",
+               "ru" => "Ruthenium",
+               "s" => "Sulfur",
+               "sb" => "Antimony",
+               "sc" => "Scandium",
+               "se" => "Selenium",
+               "sg" => "Seaborgium",
+               "si" => "Silicon",
+               "sm" => "Samarium",
+               "sn" => "Tin",
+               "sr" => "Strontium",
+               "ta" => "Tantalum",
+               "tb" => "Terbium",
+               "tc" => "Technetium",
+               "te" => "Tellurium",
+               "th" => "Thorium",
+               "ti" => "Titanium",
+               "tl" => "Thallium",
+               "tm" => "Thulium",
+               "ts" => "Tennessine",
+               "u" => "Uranium",
+               "v" => "Vanadium",
+               "w" => "Tungsten",
+               "xe" => "Xenon",
+               "y" => "Yttrium",
+               "yb" => "Ytterbium",
+               "zn" => "Zinc",
+               "zr" => "Zirconium",
+            },
         }
+    }
+
+    fn gen_compound(&self, s: &str) -> Option<Vec<&'static str>> {
+        assert!(s.is_ascii());
+        let len = s.len();
+        let mut sol = vec![None; s.len() + 1];
+        // init recurrence-relation base cases
+        // end of the string is a valid solution
+        sol[len] = Some(vec![]);
+        if let Some(&elem) = self.periodic_elements.get(&s[(len - 1)..]) {
+            sol[len - 1] = Some(vec![elem])
+        }
+
+        for i in (0..(len - 1)).rev() {
+            let fst_sol = &sol[i + 1];
+            let snd_sol = &sol[i + 2];
+            let fst = self.periodic_elements.get(&s[i..(i + 1)]);
+            let snd = self.periodic_elements.get(&s[i..(i + 2)]);
+            match (fst_sol, fst, snd_sol, snd) {
+                (Some(fst_sol), Some(&elem_one), Some(snd_sol), Some(&elem_two)) => {
+                    // favor shorter compounds
+                    sol[i] = Some(if fst_sol.len() < snd_sol.len() {
+                        appended_vec(fst_sol, elem_one)
+                    } else {
+                        appended_vec(snd_sol, elem_two)
+                    })
+                }
+                (Some(prev_sol), Some(&elem), _, _) | (_, _, Some(prev_sol), Some(&elem)) => {
+                    sol[i] = Some(appended_vec(prev_sol, elem))
+                }
+                // item already init to None, continue
+                _ => (),
+            }
+        }
+
+        sol[0]
+            .take()
+            .map(|s| s.into_iter().rev().collect::<Vec<_>>())
     }
 
     fn print_matches(&self, line: String, show_misses: bool) {
-        let mut full_match = true;
-        let mut prev_was_double = false;
-        let mut compound = Compound(Vec::new());
-        for n in 0..line.to_lowercase().chars().count() {
-            if prev_was_double {
-                prev_was_double = false;
-                continue;
-            }
-            let this_char = line.chars().nth(n).unwrap_or('?');
-            let next_char = line.chars().nth(n + 1).unwrap_or('*');
-            let mut the_double: String = "".to_string();
-            the_double.push(this_char);
-            the_double.push(next_char);
-            let mut the_single: String = "".to_string();
-            the_single.push(this_char);
-
-            if let Some(single) = self.periodic_elements.get(the_single.as_str()) {
-                compound.push(single.to_string());
-            } else if let Some(double) = self.periodic_elements.get(the_double.as_str()) {
-                compound.push(double.to_string());
-                prev_was_double = true;
-            } else {
-                full_match = false;
-            }
-        }
-        if full_match {
+        let maybe_compound = self.gen_compound(line.as_str());
+        if let Some(compound) = maybe_compound {
+            let compound = Compound(compound.into_iter().map(ToString::to_string).collect());
             println!("{} {}", line, compound);
         } else if show_misses {
-            println!("\"{}\" can't be written with the periodic table until we discover more elements.", line);
+            println!(
+                "\"{}\" can't be written with the periodic table until we discover more elements.",
+                line
+            );
         }
+    }
+}
+
+#[inline]
+fn appended_vec(base_vec: &Vec<&'static str>, elem: &'static str) -> Vec<&'static str> {
+    let mut vec = base_vec.clone();
+    vec.push(elem);
+    vec
+}
+
+#[cfg(test)]
+mod tests {
+    use super::PeriodicWords;
+
+    #[test]
+    fn dyn_chemical_compound_works() {
+        let test = PeriodicWords::new();
+        assert_eq!(
+            test.gen_compound("farts"),
+            Some(vec!["Fluorine", "Argon", "Tennessine"])
+        );
+        assert_eq!(
+            test.gen_compound("fun"),
+            Some(vec!["Fluorine", "Uranium", "Nitrogen"])
+        );
+        assert_eq!(
+            test.gen_compound("brew"),
+            Some(vec!["Boron", "Rhenium", "Tungsten"])
+        );
+        assert_eq!(test.gen_compound("ew"), None);
+        assert_eq!(
+            test.gen_compound("carbon"),
+            Some(vec!["Calcium", "Rubidium", "Oxygen", "Nitrogen"])
+        );
+        assert_eq!(
+            test.gen_compound("xenon"),
+            Some(vec!["Xenon", "Nobelium", "Nitrogen"])
+        );
+        assert_eq!(
+            test.gen_compound("silver"),
+            Some(vec!["Silicon", "Livermorium", "Erbium"])
+        );
+        assert_eq!(
+            test.gen_compound("oganesson"),
+            Some(vec![
+                "Oxygen", "Gallium", "Neon", "Sulfur", "Sulfur", "Oxygen", "Nitrogen"
+            ])
+        );
+        assert_eq!(
+            test.gen_compound("copper"),
+            Some(vec!["Cobalt", "Phosphorus", "Phosphorus", "Erbium"])
+        );
+        assert_eq!(
+            test.gen_compound("silicon"),
+            Some(vec!["Silicon", "Lithium", "Cobalt", "Nitrogen"])
+        );
+        assert_eq!(test.gen_compound("tin"), Some(vec!["Titanium", "Nitrogen"]));
+        assert_eq!(
+            test.gen_compound("dynamite"),
+            Some(vec![
+                "Dysprosium",
+                "Nitrogen",
+                "Americium",
+                "Iodine",
+                "Tellurium"
+            ])
+        );
+        assert_eq!(test.gen_compound("zummer"), None);
     }
 }
